a5c5ca04d1758568b7994c3e6409bc42e5453dfb
Oniso20 <emem.c.onis@gmail.com>
2022-08-17 12:15:17 +0300

    Odd and Even Solution

---
+++ week_2/odd_even/main.js
@@ -0,0 +1,13 @@
+const oddOrEven = () => {
+    const number = prompt("Input a number");
+
+    if (number < 0) {
+        return console.log("Enter a positive number");
+    } else if (number % 2 == 0) {
+        return console.log(number, "is even");
+    } else {
+        return console.log(number, "is odd");
+    }
+};
+
+oddOrEven();
