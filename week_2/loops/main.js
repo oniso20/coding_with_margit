7870f861e5c937346bbdd35274fd9a1ce0d3fafe
Oniso20 <emem.c.onis@gmail.com>
2022-08-23 12:54:37 +0300

    refactored loops task

--- week_2/loops/main.js
+++ week_2/loops/main.js
@@ -15,90 +15,129 @@
 // }
 // console.log(result);
 
+// Second Pattern
+
+// let result = '';
+// let end = 98;
+
+// for (let i = 2; i < 100; i += 2) {
+//     result += ' ' + i + ' ';
+//     result += ' ' + end + ' ';
+//     end -= 2;
+// }
+// console.log(result);
+
 
 // 3. Make a program that asks repeatedly from the user the distance (km) and time (h) and calculates average speed. The program ends when user gives 0 for the distance. (After getting 0, the program does not ask anything from the user.)
 
-// let distance, time 
+// let distance, time;
 
 // do {
-//     distance = +prompt("What is the distance in Km ?");
-//     if ( distance > 0 ) {
+//     distance = +prompt("What is the distance in km ?");
+//     if (distance > 0) {
 //         time = +prompt("What is the time in hrs ?");
+//     } else if (distance === 0) {
+//         console.log('Ending now');
 //     }
 //     console.log(`Average speed is: 12
-//     ${distance/time}`)
+//     ${distance / time}km/h`);
 // }
+// while (distance != 0);
 
-// while (distance != 0)
+// Second Solution
+
 
 // 4. Make a program that asks 20 numbers from user. After that the program prints out how many of those numbers where even.
 
-// let promptN, odd = 0, even = 0, num = 1
+// let odd = 0, even = 0, num = 1;
 
-// while ( num <= 20 ) {
+// while (num <= 20) {
 //     if (prompt('Enter a number') % 2 === 0) {
-//         even++
-//     } else{
-//         odd++
+//         even++;
+//     } else {
+//         odd++;
 //     }
-//     num++
+//     num++;
 // }
 
-// console.log(`Total Even numbers: ${even} and Odd: ${odd}`)
+// console.log(`Total Even numbers: ${even} and Odd: ${odd}`);
+
 
 // 5. Make a program that asks numbers from the user, until user gives 0 and then program ends. In the end program prints out average of the numbers.
 
-// let num = 0, promptN;
+// let count = 0, num = 0, promptForNum;
 
 // do {
-//     promptN = +(prompt('Enter a number'))
-//     num += promptN;
+//     promptForNum = +(prompt('Enter a number: (0 to end)'));
+//     num += promptForNum;
+//     count++;
 // }
 
-// while (promptN != 0)
+// while (promptForNum !== 0);
 
-// console.log(num/2)
+// average = Math.round(num / count);
+
+// console.log(`The average is: ${average}, the tootal number(s) entered is ${count}`);
 
 //6. Make a program that asks 25 numbers form the user. In the end program prints out average of the numbers.
 
-// let numCount = 1, numPrompt = 0;
+// let count = 0, numCount = 1, numPrompt = 0;
 // while (numCount <= 25) {
-//      numPrompt += +(prompt('Enter a number'));
-//      numCount++
+//     numPrompt += +(prompt('Enter at least 25 numbers'));
+//     numCount++;
+//     count++;
 // }
 
-// console.log(`The average is: ${numPrompt/2}`)
+// average = Math.round(numPrompt / count);
 
-// Make a program that ask first one number from the user. After that the program asks: ”Do you want to continue giving numbers?(y/n)”. If user answers y, the program continues to ask another number. If user answers n, program ends. In the end program prints out average of the numbers.
+// console.log(`The average of your inputted numbers is: ${average}`);
 
-// let num = 0, questionPrompt, numPrompt;
+// Try at home
+
+// let count = 0, numCount = 1, numPrompt = 0;
+
+// for (let i = 0; i < 25; i++) {
+//     let numPrompt += +(prompt('Enter at least 25 numbers'));
+//     numCount++;
+//     count++;
+
+// }
+// average = Math.round(numPrompt / count);
+// console.log(`The average of your inputted numbers is: ${average}`);
+
+// 7. Make a program that ask first one number from the user. After that the program asks: ”Do you want to continue giving numbers?(y/n)”. If user answers y, the program continues to ask another number. If user answers n, program ends. In the end program prints out average of the numbers.
+
+// let count = 0, num = 0, questionPrompt, numPrompt;
 
 // do {
-//     numPrompt = +prompt('Enter a number: ')
-//     num += numPrompt
-//     questionPrompt = prompt('Do you want to continue giving numbers?(y/n)').toLowerCase()
+//     numPrompt = +prompt('Enter a number: ');
+//     num += numPrompt;
+//     count++;
+//     questionPrompt = prompt('Do you want to continue giving numbers?(y/n)').toLowerCase();
 // }
 
-// while (questionPrompt === 'y')
+// while (questionPrompt === 'y');
 
-// console.log(`The average is: ${num/2}`)
+// average = Math.round(num / count);
+
+// console.log(`The average is: ${average} and the total number(s) inputted is ${count}`);
 
 // 8. Make a program that asks first how many numbers user wants to give to the program. After that program asks those numbers. In the end program prints out the smallest number that user gave.
 
-// let num, trackNum = 1;
-// let numCount = +prompt('How many numbers do you want?')
-// let newArr = [];
+let num, trackNum = 1;
+let numCount = +prompt('How many numbers do you want?');
+let newArr = [];
 
-// do {
-//     num = +prompt(`Enter number "${trackNum}" here:`)
-//     trackNum++
-//     numCount--
-//     newArr.push(num)
+do {
+    num = +prompt(`Enter number "${trackNum}" here:`);
+    trackNum++;
+    numCount--;
+    newArr.push(num);
 
-// }
+}
 
-// while (numCount > 0)
-// console.log(`The maximum number inputed is: ${Math.max(...newArr)}`)
+while (numCount > 0);
+console.log(`The maximum number inputted is: ${Math.max(...newArr)}`);
 
 // 9. Make a program that asks ten numbers and in the end prints out two biggest numbers.
 
@@ -116,30 +155,30 @@
 
 // while (numCount > 0)
 // sortedArr = newArr.sort((a,b) => b - a)
-// console.log(`The inputed numbers are: ${ newArr }
-// The biggest numbers inputed are: ${ sortedArr[0] } and ${ sortedArr[1] }`)
+// console.log(`The inputted numbers are: ${ newArr }
+// The biggest numbers inputted are: ${ sortedArr[0] } and ${ sortedArr[1] }`)
 
 // 10. Make a program that asks ten numbers. Program calculates and prints out sum and average, also prints out the smallest and biggest number.
 
-let num, trackNum = 1;
-let numCount = 10
-let newArr = [];
+// let num, trackNum = 1;
+// let numCount = 10
+// let newArr = [];
 
-do {
-    num = +prompt(`Enter number "${trackNum}" here:`)
-    trackNum++
-    numCount--
-    newArr.push(num)
+// do {
+//     num = +prompt(`Enter number "${trackNum}" here:`)
+//     trackNum++
+//     numCount--
+//     newArr.push(num)
 
-}
+// }
 
-while (numCount > 0)
-maxNum = Math.max(...newArr)
-minNum = Math.min(...newArr)
-sumOfArr = newArr.reduce((cum, curr) => cum + curr, 0)
-avgNumber = sumOfArr/newArr.length
-console.log(` The inputed numbers are: ${ newArr }
-The Max number is: ${ maxNum }
-The Min number is: ${ minNum }
-The Sum of the numbers is: ${ sumOfArr }
-The Average of the numbers is: ${ avgNumber } `)
+// while (numCount > 0)
+// maxNum = Math.max(...newArr)
+// minNum = Math.min(...newArr)
+// sumOfArr = newArr.reduce((cum, curr) => cum + curr, 0)
+// avgNumber = sumOfArr/newArr.length
+// console.log(` The inputted numbers are: ${ newArr }
+// The Max number is: ${ maxNum }
+// The Min number is: ${ minNum }
+// The Sum of the numbers is: ${ sumOfArr }
+// The Average of the numbers is: ${ avgNumber } `)
