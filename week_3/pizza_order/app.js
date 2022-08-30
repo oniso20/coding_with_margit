c755e74918a5d79749d3b6544bd33a12b852acee
Oniso20 <emem.c.onis@gmail.com>
2022-08-30 14:59:30 +0300

    Maggit's pizza order solution

---
+++ week_3/pizza_order/app.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('form');
+const customer = document.querySelector('#name');
+const size = document.querySelectorAll('[name="size"');
+const toppings = document.querySelectorAll('input[type="checkbox"');
+const delivery = document.querySelector('#delivery');
+const order = document.querySelector('#result');
+
+const takeOrder = (event) => {
+    event.preventDefault();
+
+    let customerName = customer.value;
+    let sizeResult = '';
+    let toppingResult = [];
+    let deliveryResult = delivery.options[delivery.selectedIndex].value;
+    let price = 0;
+
+    size.forEach(item => {
+        if (item.checked) {
+            sizeResult = item.value;
+        }
+    });
+
+    switch (sizeResult) {
+        case 'two':
+            price += 7.5;
+            break;
+        case 'four':
+            price += 10.5;
+            break;
+        case 'six':
+            price += 12.5;
+            break;
+        case 'eight':
+            price += 15.5;
+            break;
+        default:
+    }
+
+    toppings.forEach(item => {
+        if (item.checked) {
+            toppingResult.push(item.value);
+        }
+    });
+
+    if (toppingResult.length > 4) {
+        price += (toppingResult.length - 4) * 0.5;
+    }
+
+    if (deliveryResult == 'home') {
+        price += 5;
+    }
+
+    order.textContent = `Name: <span>${customerName}</span>, Size: ${sizeResult}, Topping: ${toppingResult.join(', ')} and delivery method is: ${deliveryResult}. Total price is: ${price} €.`;
+    console.log(customerName);
+    console.log(sizeResult);
+    console.log(toppingResult);
+    console.log(deliveryResult);
+    console.log(price);
+};
+
+form.addEventListener('submit', takeOrder);
